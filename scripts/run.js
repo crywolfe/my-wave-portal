beb0817e642419888fdf5edec6d7fab4fe66107f
Gerry Wolfe <gwolfe@slync.io>
2022-03-26 17:37:40 -0700

    add ether

--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,37 @@
 const main = async () => {
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
   await waveContract.deployed();
-  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract address is: ", waveContract.address);
+
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:", 
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
   console.log(waveCount.toNumber());
 
-  let waveTxn = await waveContract.wave("FIRST MESSAGE");
+  let waveTxn = await waveContract.wave("A MESSAGE");
   await waveTxn.wait();
 
-  const [_, randomPerson] = await hre.ethers.getSigners();
-  waveTxn = await waveContract.connect(randomPerson).wave("Random's message");
-  await waveTxn.wait();
+  contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:", 
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  // const [_, randomPerson] = await hre.ethers.getSigners();
+  // waveTxn = await waveContract.connect(randomPerson).wave("Random's message");
+  // await waveTxn.wait();
 
   let allWaves = await waveContract.getAllWaves();
   console.log(allWaves);
